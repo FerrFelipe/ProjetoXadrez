4587e01e5f105ba6ea3df7209d6dab363a2fe308
Felipe Ferreira <fefelipe21@gmail.com>
2024-05-05 14:25:12 -0300

    Tratando erros com excessao personalizada.

--- ProjetoXadrez/Tabuleiro/Tabuleiro.cs
+++ ProjetoXadrez/Tabuleiro/Tabuleiro.cs
@@ -11,14 +11,42 @@
             this.colunas = colunas;
             pecas = new Peca[linhas, colunas];
         }
+
         public Peca peca(int linhas, int colunas)
         {
             return pecas[linhas, colunas];
         }
+
+        public Peca peca(Posicao pos)
+        {
+            return pecas[pos.linha, pos.coluna];
+        }
+
+        public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return peca(pos) != null;
+        }
+
         public void colocarPeca(Peca p, Posicao pos)
         {
+            if (existePeca(pos)) throw new TabuleiroException("Já existe uma peca nessa posicao.");
             pecas[pos.linha, pos.coluna] = p;
             p.posicao = pos;
+        }
+
+        public bool posicaoValida(Posicao pos)
+        {
+            if (pos.linha < 0 || pos.coluna < 0 || pos.linha >= linhas || pos.coluna >= colunas) return false;
+            return true;
+        }
+
+        public void validarPosicao(Posicao pos)
+        {
+            if (!posicaoValida(pos))
+            {
+                throw new TabuleiroException("Posicao Invalida!");
+            }
         }
     }
 }
