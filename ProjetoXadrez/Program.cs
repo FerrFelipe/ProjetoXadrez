4fce36f2a4d934767d55e586d6980f87b80fbb36
Felipe Ferreira <fefelipe21@gmail.com>
2024-05-05 13:37:25 -0300

    Criado a classe Posicao

--- ProjetoXadrez/Program.cs
+++ ProjetoXadrez/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tabuleiro;
+
+namespace ProjetoXadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
